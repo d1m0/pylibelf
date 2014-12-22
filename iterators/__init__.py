688894dad19bd3e9e8d9771e963a39113cfbb4ff
Dimitar Bounov <dbounov@cs.ucsd.edu>
2014-12-22 14:41:19 -0800

    sections, section headers iterators

--- iterators/__init__.py
+++ iterators/__init__.py
@@ -1,5 +1,34 @@
 import sys
 import os
-import libelf.types
-import libelf.constants
+import libelf
+from libelf import *
+from libelf.constants import *
 
+def _class(elf):  elf_getident(elf, None)[EI_CLASS]
+
+def _is32(elf):   _class(elf) == ELFCLASS32 
+def _is64(elf):   _class(elf) == ELFCLASS64 
+
+def select(elf, fname):
+  if _is32(elf):
+    return libelf.__getattribute__('elf32_' + fname)
+  else:
+    return libelf.__getattribute__('elf64_' + fname)
+
+def sections(elf):
+  i = None
+  while 1:
+    i = elf_nextscn(elf, i)
+    if (not bool(i)):
+      break
+
+    yield i.contents
+
+def shdrs(elf):
+  i = None
+  while 1:
+    i = elf_nextscn(elf, i)
+    if (not bool(i)):
+      break
+
+    yield select(elf, 'getshdr')(i.contents).contents
